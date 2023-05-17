e007b607c9debdd0081e0b0413923fb492fa8325
nmorduch <1051450+nmorduch@users.noreply.github.com>
2023-05-17 16:36:16 -0400

    Show multiple questions

--- src/dashboard.js
+++ src/dashboard.js
@@ -12,73 +12,85 @@ marked.use({
 export default function (el, data) {
   const viz = d3.select(el).html(`
   <div class="dv-header">
-    <h3>Pathways into Teaching</h3>
+    <h1>Pathways into Teaching</h1>
     <!--div>description TK</div-->
     <!--div class="dv-filters"></div-->
   </div>
   <div class="dv-main">
-    <div class="dv-question">
-      <ul class="dv-legend"></ul>
-      <div class="dv-pathways-container">
-        <div class="dv-pathways"></div>
-        <div class="dv-info"></div>
-      </div>
+    <div class="dv-sidebar">
+      <div class="dv-filters"></div>
+      <div class="dv-info"></div>
+    </div>
+    <div class="dv-questions"></div>
     </div>
   </div>
   `)
 
-  let current_question = data.questions[0]
-  let current_question_code = current_question.question_code
-  let current_answers = data.answers.filter(a => a.question == current_question_code)
-
-  viz.select('.dv-legend')
-    .selectAll('li')
-    .data(current_answers)
-    .join('li')
-      .html(d => d.answer_text)
-      .style('background-color', d => colors[d.color].lightest)
-      .style('color', d => colors[d.color].darkest)
-
-  viz.select('.dv-pathways')
+  const questions = viz.select('.dv-questions')
     .selectAll('div')
-    .data(data.pathways)
+    .data(data.questions)
     .join('div')
-      .classed('dv-pathway', true)
-      .html(d => d["usps"])
-      .sort((a, b) => current_answers.findIndex(an => an['answer_code'] == a[current_question_code]) - current_answers.findIndex(an => an['answer_code'] == b[current_question_code]))
-      .style('background-color', d => colors[(current_answers.find(a => a.answer_code == d[current_question_code]) ? current_answers.find(a => a.answer_code == d[current_question_code]).color : 'grey')].light)
-      .on("click", onclick)
-  
-  let selectedPathway
+
+  questions
+    .append('h3')
+      .html(d => d.question_text)
+        
+  questions.each(function(q) {
+    let current_question_code = q.question_code
+    let current_answers = data.answers.filter(a => a.question == current_question_code)
+    d3.select(this).append('div')
+      .classed('dv-legend', true)
+      .selectAll('li')
+      .data(current_answers)
+      .join('li')
+        .html(d => d.answer_text)
+        .style('background-color', d => colors[d.color].lightest)
+      .style('color', d => colors[d.color].darkest)
+    d3.select(this).append('div')
+      .classed('dv-pathways', true)
+      .selectAll('div')
+      .data(data.pathways)
+      .join('div')
+        // .attr('color', d => current_answers.find(a => a.answer_code == d[current_question_code]) ? current_answers.find(a => a.answer_code == d[current_question_code]).color : 'grey')
+        .classed('dv-pathway', true)
+        .html(d => d["usps"])
+        .sort((a, b) => current_answers.findIndex(an => an['answer_code'] == a[current_question_code]) - current_answers.findIndex(an => an['answer_code'] == b[current_question_code]))
+        .style('background-color', d => colors[(current_answers.find(a => a.answer_code == d[current_question_code]) ? current_answers.find(a => a.answer_code == d[current_question_code]).color : 'grey')][d.isSelected ? 'darkest' : 'light'])
+        .on("click", onclick)
+        
+      })
+      
+      
   let infoBox = viz.select('.dv-info')
 
-  const selectPathway = (d) => {
-    selectedPathway
-      .style('background-color', d => colors[(current_answers.find(a => a.answer_code == d[current_question_code]) ? current_answers.find(a => a.answer_code == d[current_question_code]).color : 'grey')].darkest)
-      .style('color', 'white')
+
+  const highlightPathway = (d) => {
+    d.isSelected = true
+    viz.selectAll('.dv-pathway')
+      .filter(p => p.isSelected)
+        // .style('color', 'white')
+        .style('font-weight', 'bold')
+
     infoBox.html(`
       <div>${d.state}</div>
       <div>${marked.parseInline(d.pathway)}</div>
       <div>Credential: ${marked.parseInline(d.credential)}</div>
-      <div>${current_question.question_text}: ${d[current_question_code]}</div>
-    `)
+      `)
+      // <div>${current_question.question_text}: ${d[current_question_code]}</div>
   }
-  const unselectPathway = (d) => {
-    selectedPathway
-      .style('background-color', d => colors[(current_answers.find(a => a.answer_code == d[current_question_code]) ? current_answers.find(a => a.answer_code == d[current_question_code]).color : 'grey')].light)
-      .style('color', null)
-      infoBox.html('Click on a pathway for more information.')
-    }
+  const unhighlightPathway = () => {
+    viz.selectAll('.dv-pathway')
+      .filter(d => d.isSelected)
+      // .style('color', null)
+      .style('font-weight', 'normal')
+      .datum(d => d.isSelected = false)
+    infoBox.html('Click on a pathway for more information.')
+  }
     
   function onclick(e, d) {
-    if (selectedPathway) {
-      unselectPathway(selectedPathway.datum())
-      if (selectedPathway.datum() == d) {
-        selectedPathway = null
-        return
-      }
+    unhighlightPathway()
+    if (!d.isSelected) {
+      highlightPathway(d)
     }
-    selectedPathway = d3.select(this)
-    selectPathway(selectedPathway.datum())
   }
 }
